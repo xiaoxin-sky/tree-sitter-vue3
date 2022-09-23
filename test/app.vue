230d4a02707ee58bf3db82c6b7badc5e725f6be2
lipengze <779931346@qq.com>
2022-09-23 11:49:10 +0800

    fix: func name

---
+++ test/app.vue
@@ -0,0 +1,26 @@
+<template>
+  <div :class="[env.isDarkTheme ? 'night' : 'day']">
+    <Suspense>
+      <template #default>
+        <router-view />
+      </template>
+      <template #fallback>
+        <div>加载中...</div>
+      </template>
+    </Suspense>
+  </div>
+</template>
+
+<script lang="ts">
+import { env } from "gkoudai-private-front-lib";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  components: {},
+  setup() {
+    return { env };
+  },
+});
+</script>
+
+<style lang="scss"></style>
