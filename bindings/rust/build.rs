96729e0594ab45a17a2e24e865bbf3f788c54dd1
lipengze <779931346@qq.com>
2022-09-23 13:51:02 +0800

    fix: rust build fail

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -1,14 +1,14 @@
 fn main() {
     let src_dir = std::path::Path::new("src");
 
-    // let mut c_config = cc::Build::new();
-    // c_config.include(&src_dir);
-    // c_config
-    //     .flag_if_supported("-Wno-unused-parameter")
-    //     .flag_if_supported("-Wno-unused-but-set-variable")
-    //     .flag_if_supported("-Wno-trigraphs");
-    // let parser_path = src_dir.join("parser.c");
-    // c_config.file(&parser_path);
+    let mut c_config = cc::Build::new();
+    c_config.include(&src_dir);
+    c_config
+        .flag_if_supported("-Wno-unused-parameter")
+        .flag_if_supported("-Wno-unused-but-set-variable")
+        .flag_if_supported("-Wno-trigraphs");
+    let parser_path = src_dir.join("parser.c");
+    c_config.file(&parser_path);
 
     // If your language uses an external scanner written in C,
     // then include this block of code:
@@ -19,8 +19,8 @@ fn main() {
     println!("cargo:rerun-if-changed={}", scanner_path.to_str().unwrap());
     */
 
-    // c_config.compile("parser");
-    // println!("cargo:rerun-if-changed={}", parser_path.to_str().unwrap());
+    c_config.compile("parser");
+    println!("cargo:rerun-if-changed={}", parser_path.to_str().unwrap());
 
     // If your language uses an external scanner written in C++,
     // then include this block of code:
